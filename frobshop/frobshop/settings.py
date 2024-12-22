68331a2315b75d464146d54fe6346ed49fce4b8d
csutton2020 <physicsudel2011@gmail.com>
2024-12-21 21:33:59 -0500

    Update settings.py
    added media root, media url based on sandbox settings. need to look into this some more

--- frobshop/frobshop/settings.py
+++ frobshop/frobshop/settings.py
@@ -175,6 +175,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = location("public/media")
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
