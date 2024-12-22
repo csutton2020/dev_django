eac358cd154fe802d9bd121095052f8a539d59e0
csutton2020 <physicsudel2011@gmail.com>
2024-12-21 19:39:50 -0500

    Update urls.py
    remove language URL. Checked settings.py and one EN-us was used, which is what I want

--- frobshop/frobshop/urls.py
+++ frobshop/frobshop/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-     path('i18n/', include('django.conf.urls.i18n')),
+     #path('i18n/', include('django.conf.urls.i18n')),
 
     # The Django admin is not officially supported; expect breakage.
     # Nonetheless, it's often useful for debugging.
