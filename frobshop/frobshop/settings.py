b4039e4085d04add4270ae25399068135fbb496f
csutton2020 <physicsudel2011@gmail.com>
2024-12-21 21:35:58 -0500

    Update settings.py
    added static root and static url per sandbox

--- frobshop/frobshop/settings.py
+++ frobshop/frobshop/settings.py
@@ -173,7 +173,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = location('public/static')
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
